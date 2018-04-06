866bfcb59ea208bd570ec4fec59ca526559d6d07
Martin Martinka <martin.martinka@gmail.com>
2018-04-06 22:10:41 +0200

    Added Lombok functionality

--- src/main/java/com/vaadin/starter/beveragebuddy/backend/Ziadatel.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/backend/Ziadatel.java
@@ -6,9 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Represents a beverage category.
  */
+@Getter @Setter @EqualsAndHashCode @ToString
 public class Ziadatel implements Serializable {
 
 	// URL;Ziadatel;ICO;Rok;Lokalita;Diel;Kultura;Vymera
@@ -34,149 +40,4 @@ public class Ziadatel implements Serializable {
 	private BigDecimal vymera;
 
 	private List<ZiadostDiely> listZiadostDiely = new ArrayList<>();
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getZiadatel() {
-		return ziadatel;
-	}
-
-	public void setZiadatel(String ziadatel) {
-		this.ziadatel = ziadatel;
-	}
-
-	public String getIco() {
-		return ico;
-	}
-
-	public void setIco(String ico) {
-		this.ico = ico;
-	}
-
-	public String getRok2005() {
-		return rok2005;
-	}
-
-	public void setRok2005(String rok2005) {
-		this.rok2005 = rok2005;
-	}
-
-	public String getRok2006() {
-		return rok2006;
-	}
-
-	public void setRok2006(String rok2006) {
-		this.rok2006 = rok2006;
-	}
-
-	public String getRok2007() {
-		return rok2007;
-	}
-
-	public void setRok2007(String rok2007) {
-		this.rok2007 = rok2007;
-	}
-
-	public String getRok2008() {
-		return rok2008;
-	}
-
-	public void setRok2008(String rok2008) {
-		this.rok2008 = rok2008;
-	}
-
-	public String getRok2009() {
-		return rok2009;
-	}
-
-	public void setRok2009(String rok2009) {
-		this.rok2009 = rok2009;
-	}
-
-	public String getRok2010() {
-		return rok2010;
-	}
-
-	public void setRok2010(String rok2010) {
-		this.rok2010 = rok2010;
-	}
-
-	public String getRok2011() {
-		return rok2011;
-	}
-
-	public void setRok2011(String rok2011) {
-		this.rok2011 = rok2011;
-	}
-
-	public String getRok2012() {
-		return rok2012;
-	}
-
-	public void setRok2012(String rok2012) {
-		this.rok2012 = rok2012;
-	}
-
-	public String getRok2013() {
-		return rok2013;
-	}
-
-	public void setRok2013(String rok2013) {
-		this.rok2013 = rok2013;
-	}
-
-	public String getRok2014() {
-		return rok2014;
-	}
-
-	public void setRok2014(String rok2014) {
-		this.rok2014 = rok2014;
-	}
-
-	public String getRok2015() {
-		return rok2015;
-	}
-
-	public void setRok2015(String rok2015) {
-		this.rok2015 = rok2015;
-	}
-
-	public String getRok2016() {
-		return rok2016;
-	}
-
-	public void setRok2016(String rok2016) {
-		this.rok2016 = rok2016;
-	}
-
-	public String getRok2017() {
-		return rok2017;
-	}
-
-	public void setRok2017(String rok2017) {
-		this.rok2017 = rok2017;
-	}
-
-	public BigDecimal getVymera() {
-		return vymera;
-	}
-
-	public void setVymera(BigDecimal vymera) {
-		this.vymera = vymera;
-	}
-
-	public List<ZiadostDiely> getListZiadostDiely() {
-		return listZiadostDiely;
-	}
-
-	public void setListZiadostDiely(List<ZiadostDiely> listZiadostDiely) {
-		this.listZiadostDiely = listZiadostDiely;
-	}
-
 }
--- src/main/java/com/vaadin/starter/beveragebuddy/backend/ZiadostDiely.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/backend/ZiadostDiely.java
@@ -1,11 +1,19 @@
 package com.vaadin.starter.beveragebuddy.backend;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Represents a beverage category.
  */
+@Getter @Setter @EqualsAndHashCode @ToString
 public class ZiadostDiely implements Serializable {
 
 	// URL;Ziadatel;ICO;Rok;Lokalita;Diel;Kultura;Vymera
@@ -19,102 +27,4 @@ public class ZiadostDiely implements Serializable {
 	private String diel;
 	private String kultura;
 	private String vymera;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getZiadatel() {
-		return ziadatel;
-	}
-
-	public void setZiadatel(String ziadatel) {
-		this.ziadatel = ziadatel;
-	}
-
-	public String getIco() {
-		return ico;
-	}
-
-	public void setIco(String ico) {
-		this.ico = ico;
-	}
-
-	public String getRok() {
-		return rok;
-	}
-
-	public void setRok(String rok) {
-		this.rok = rok;
-	}
-
-	public String getLokalita() {
-		return lokalita;
-	}
-
-	public void setLokalita(String lokalita) {
-		this.lokalita = lokalita;
-	}
-
-	public String getDiel() {
-		return diel;
-	}
-
-	public void setDiel(String diel) {
-		this.diel = diel;
-	}
-
-	public String getKultura() {
-		return kultura;
-	}
-
-	public void setKultura(String kultura) {
-		this.kultura = kultura;
-	}
-
-	public String getVymera() {
-		return vymera;
-	}
-
-	public void setVymera(String vymera) {
-		this.vymera = vymera;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ZiadostDiely other = (ZiadostDiely) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
 }
