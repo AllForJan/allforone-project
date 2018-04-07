37fb51e2fac265bd877a2bf48df42ef12f45365e
Jozef Snirc <jozef.snirc@gmail.com>
2018-04-07 23:05:04 +0200

    klient

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/views/poberatelia/ViewPoberateliaList.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/views/poberatelia/ViewPoberateliaList.java
@@ -6,14 +6,17 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.starter.beveragebuddy.backend.Ziadatel;
+import com.vaadin.starter.beveragebuddy.backend.rpvs.FirmaPlatby;
 import com.vaadin.starter.beveragebuddy.backend.rpvs.PoberatelSumar;
 import com.vaadin.starter.beveragebuddy.backend.rpvs.RpvsService;
 import com.vaadin.starter.beveragebuddy.ui.MainLayout;
@@ -129,14 +132,23 @@ public class ViewPoberateliaList extends VerticalLayout {
 		grid.addColumn(poberatel -> poberatel.getSumaVsetkychPlatieb()).setHeader("Celková suma").setResizable(true)
 				.setSortable(true);
 
+		grid.setItemDetailsRenderer(new ComponentRenderer<>(poberatel -> {
+			VerticalLayout layout = new VerticalLayout();
+			for (FirmaPlatby fp : poberatel.getPlatbyFirmam()) {
+				layout.add(new Label(fp.getNazov() + " - " + fp.getIco() + ""));
+				// layout.add(new Label("Year of birth: " + person.getYearOfBirth()));
+			}
+			return layout;
+		}));
+
 	}
 
 	private void createYearColumns() {
 		for (int i = from; i <= to; i++) {
 			final int rok = i;
-			grid.addColumn(ziadatel -> ziadatel.getSumaPlatieb(rok))		
-					.setHeader("Platby "+rok).setKey(rok + "").setWidth("3em").setResizable(true);
-			
+			grid.addColumn(ziadatel -> ziadatel.getSumaPlatieb(rok)).setHeader("Platby " + rok).setKey(rok + "")
+					.setWidth("3em").setResizable(true);
+
 		}
 
 	}
